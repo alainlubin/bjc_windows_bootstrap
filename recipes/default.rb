5f4cca30f59820f35b01b6b4849b1d22ee446bf1
Anthony Rees <anthony_rees01@hotmail.com>
2019-03-18 19:42:38 +1100

    ip

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,7 @@
 #
 powershell_script 'Set host file so the instance knows where to find chef-server' do
     code <<-EOH
-    $hosts = "54.218.105.168 chef.automate-demo.com"
+    $hosts = "34.219.147.10 chef.automate-demo.com"
     $file = "C:\\Windows\\System32\\drivers\\etc\\hosts"
     $hosts | Add-Content $file
     EOH
