ff1dd1fbb42c11a7fba8e92bf1fe8fab87c60c0f
Anthony Rees <anthony_rees01@hotmail.com>
2020-04-08 15:21:49 +1000

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,24 +6,8 @@
 #
 # Validator.pem on Workstation C:\opscode\chefdk\embedded\lib\ruby\gems\2.4.0\gems\berkshelf-6.3.1\spec\config
 #
-powershell_script 'Set host file so the instance knows where to find chef-server' do
-    code <<-EOH
-    $hosts = "34.209.176.239 chef.automate-demo.com"
-    $file = "C:\\Windows\\System32\\drivers\\etc\\hosts"
-    $hosts | Add-Content $file
-    EOH
-  end
-  
-  powershell_script 'Create first-boot.json' do
-    code <<-EOH
-    $firstboot = @{
-       "run_list" = @("role[base]")
-    }
-    Set-Content -Path c:\\chef\\first-boot.json -Value ($firstboot | ConvertTo-Json -Depth 10)
-    EOH
-  end
     
-  powershell_script 'Create first-boot.json2' do
+  powershell_script 'Create config.rb' do
     code <<-EOH
     $nodeName = "Win-Ant-{0}" -f (-join ((65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_}))
   
@@ -39,9 +23,10 @@ chef_license 'accept'
     Set-Content -Path c:\\chef\\client.rb -Value $clientrb
     EOH
   end
+
   powershell_script 'Run Chef' do
     code <<-EOH
     ## Run Chef
-    C:\\opscode\\chef\\bin\\chef-client.bat -j C:\\chef\\first-boot.json
+    C:\\opscode\\chef\\bin\\chef-client.bat
     EOH
   end
